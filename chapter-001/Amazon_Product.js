092caedee239ed59c337d038302919c113f9cb70
Anjali Singh <152669219+anjali-1s@users.noreply.github.com>
2024-03-20 14:50:34 +0530

    Add files via upload

---
+++ chapter-001/Amazon_Product.js
@@ -0,0 +1,19 @@
+const product = {
+     title:"Ball Pen",
+     rating:4,
+     offer:5,
+     price:270
+};
+
+console.log(product);
+
+const profile = {
+     username:"Anjali Singh",
+     isFollow: false,
+     followers: 120,
+     following: 113
+};
+
+console.log(typeof profile["followers"]);
+console.log(typeof profile["isFollow"]);
+console.log(typeof profile["username"]);
---
+++ chapter-001/first.js
@@ -0,0 +1,66 @@
+console.log("Hello World");
+console.log("Welcome to this World!");
+
+Fullname = "Tony Stark";
+console.log(Fullname);
+
+age = 24;
+console.log(age);
+
+x = null;
+console.log(x)
+
+y = undefined;
+console.log(y)
+
+isFollow = false;
+console.log(isFollow)
+
+num1 = 20;
+num2 = 30;
+console.log(num1+num2);
+
+let name = "Anjali Singh";
+console.log(name)
+
+let price = 1000;
+console.log(price)
+
+price = 2000;
+console.log(price)
+
+var number1 = 20;
+console.log(number1)
+
+number1 = 30;
+console.log(number1)
+
+const Age = 24;
+console.log(age)
+
+const n = 0;
+
+{
+     let a = 20;
+     console.log(a)
+}
+
+{
+     let a = "name";
+     console.log(a)
+}
+
+const student = {
+     fullname:"Anjali Singh",
+     age:20,
+     marks:412
+};
+
+console.log(student.fullname);
+console.log(student["fullname"])
+
+student["age"] = student["age"] + 1;
+console.log(student["age"])
+
+student["fullname"] = "Aliyah"
+console.log(student.fullname)
